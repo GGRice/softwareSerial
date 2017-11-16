49037d2b8d7e79e816ba766eeb5ae0f8132846f6
GGRice <gretchrice4@gmail.com>
2017-11-16 13:17:13 -0500

    Code for two separate Arduinos
    This code is for the send Arduino and the receive Arduino in a Software Serial system.

---
+++ softwareSerial_Receive/softwareSerial_Receive.ino
@@ -0,0 +1,41 @@
+/**
+ * Simple example of Arduino to Arduino communication using Software Serial
+ * This is the reive Arduino. It will recieve a number from the Send Arduino
+ * and will turn the built in LED on or off accordingly.
+ * 
+ * Gretchen Rice
+ * 11/16/17
+ */
+
+#include <SoftwareSerial.h> //need this library to run Software Serial
+
+SoftwareSerial mySerial(11, 10); // set up the Software serial connection to talk to the other Arduino with (transmit pin, receive pin)
+
+bool state = LOW; //sets state of the light
+int rv = 0; //received value
+
+void setup() {
+  Serial.begin(9600); //sets up serial connection between Arduino and computer
+  mySerial.begin(4800); //sets up software serial connection between 2 Arduinos
+
+  pinMode(LED_BUILTIN, OUTPUT); //sets up light as Arduino built in light
+}
+
+
+void loop() {
+  if(mySerial.available() > 0){ //if there is a serial value 
+    Serial.println("Receiving"); //alerts us we are receiving from other Arduino
+    rv = mySerial.read(); //sets rv equal to the number we are recieving from the other Arduino
+                          //receives ints, not bytes like Hardware Serial
+    Serial.println(rv); //tells us what we recieved
+  }
+  // put your main code here, to run repeatedly:
+  if(rv==0){ //turns state off if recieved 0
+    state = LOW;
+  }
+  else if(rv==1){ //turns state on if recieved 1
+    state = HIGH;
+  }
+
+  digitalWrite(LED_BUILTIN, state); //sets state of light depending on received value
+}
---
+++ softwareSerial_Send/softwareSerial_Send.ino
@@ -0,0 +1,30 @@
+/**
+ * Simple example of Arduino to Arduino communication using Software Serial
+ * This is the sned Arduino. It will recieve a number from the Hardware Serial
+ * and will pass that reading to the Receive Arduino.
+ * 
+ * Gretchen Rice
+ * 11/16/17
+ */
+ 
+#include <SoftwareSerial.h> //need this library to run Software Serial
+
+SoftwareSerial mySerial(11, 10); // set up the Software serial connection to talk to the other Arduino with (transmit pin, receive pin)
+
+int sv = 0; //sent value
+
+void setup() {
+  Serial.begin(9600); //sets up serial connection between Arduino and computer
+  mySerial.begin(4800); //sets up software serial connection between 2 Arduinos
+}
+
+void loop() {
+  if(Serial.available() > 0) { //checks for input from Hardware Serial
+    sv = Serial.read()-'0'; //reads value from Hardware serial.
+                            //-'0' converts byte to int
+    mySerial.write(sv); //writes the value to the other Arduino
+    Serial.println(sv); //tells us the value we are sending
+  }
+  
+} 
+
